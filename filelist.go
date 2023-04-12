430ceeb03623ae8947b3d448b46f6f4fd9a357d3
conan <46959313+Conan924@users.noreply.github.com>
2023-04-12 14:56:13 +0800

    Update filelist.go
    实时输出
    多线程
    黑名单配置
    ...

--- filelist.go
+++ filelist.go
@@ -1,39 +1,91 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 )
-var linelist []string
+
+var (
+	linelist  []string
+	blacklist []string
+)
 
 func main() {
+	readConfig()
+
 	str, _ := os.Getwd()
-	filepath.Walk(str, walkfunc)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		writeToFile("filelist_output.txt")
+	}()
+
+	filepath.Walk(str, func(path string, info os.FileInfo, err error) error {
+		if isFile(path) {
+			if !isBlackListed(path) {
+				path = strings.Replace(path, str, "", -1)
+				path = strings.Replace(path, "\\", "/", -1)
+				fmt.Println(path)
+				linelist = append(linelist, path)
+				writeToFile(path)
+			}
+		}
+		return nil
+	})
+
+	wg.Wait()
 }
 
-func walkfunc(path string, info os.FileInfo, err error) error {
-	str, _ := os.Getwd()    //当前目录
-	path = strings.Replace(path, str, "", -1)
-	path = strings.Replace(path, "\\", "/", -1) //适配Windows目录
-	fmt.Println(path)
-	linelist = append(linelist,path)
-	//写入文件
-	f, err := os.Create("output.txt")
+func isFile(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func isBlackListed(path string) bool {
+	for _, suffix := range blacklist {
+		if strings.HasSuffix(strings.ToLower(path), strings.ToLower(suffix)) {
+			return true
+		}
+	}
+	return false
+}
+
+func readConfig() {
+	file, err := os.Open("filelist_config.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	// remember to close the file
-	defer f.Close()
+	defer file.Close()
 
-	for _, line := range linelist {
-		_, err := f.WriteString(line + "\n")
-		if err != nil {
-			log.Fatal(err)
-		}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		blacklist = append(blacklist, strings.ToLower(scanner.Text()))
 	}
 
-	return nil
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func writeToFile(path string) {
+	f, err := os.OpenFile("filelist_output.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(path + "\n"); err != nil {
+		log.Fatal(err)
+	}
 }
